1fdee6a7e128f6373558335ad28bc884510f5ff8
Novaspark <61843617+novaspark@users.noreply.github.com>
2025-11-11 11:01:46 +0000

    fff

--- TrelloCardEffort/effort.js
+++ TrelloCardEffort/effort.js
@@ -7,9 +7,7 @@ t.render(function () {
   const container = document.getElementById('content');
 
   return t.get('card', 'shared', 'estimatedEffort').then(function (e) {
-    console.log(e);
     t.get('card', 'shared', 'actualEffort').then(function (f) {
-      console.log(f);
       container.innerHTML = `
           <label>Estimated Effort (hours):
             <input type="number" id="est" value="${e}" />
@@ -19,16 +17,18 @@ t.render(function () {
           </label>
           <button id="save">Save</button>
         `;
+
+      document.getElementById('save').addEventListener('click', function (evt) {
+        evt.preventDefault();
+        const newEst = parseFloat(document.getElementById('est').value || 0);
+        const newAct = parseFloat(document.getElementById('act').value || 0);
+
+        t.set('card', 'shared', 'estimatedEffort', newEst);
+        t.set('card', 'shared', 'actualEffort', newAct);
+      });
     });
   });
-
-
-  document.getElementById('save').addEventListener('click', function (evt) {
-    evt.preventDefault();
-    const newEst = parseFloat(document.getElementById('est').value || 0);
-    const newAct = parseFloat(document.getElementById('act').value || 0);
-
-    t.set('card', 'shared', 'estimatedEffort', newEst);
-    t.set('card', 'shared', 'actualEffort', newAct);
-  });
 });
+
+
+
