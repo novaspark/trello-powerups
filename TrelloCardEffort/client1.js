d736db522bdc363a80ad96b88f5d275780fa47e4
Novaspark <61843617+novaspark@users.noreply.github.com>
2025-11-11 10:36:48 +0000

    f

--- TrelloCardEffort/client1.js
+++ TrelloCardEffort/client1.js
@@ -1,7 +1,6 @@
 /* global TrelloPowerUp */
 
 var Promise = TrelloPowerUp.Promise;
-const t = TrelloPowerUp.iframe();
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
